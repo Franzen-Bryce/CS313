b7f276b4b2337d105df6aeb86ed733e92fe2dc73
BryceFranzen <fra12032@byui.edu>
2015-01-08 10:50:40 -0700

    Changed Files

---
+++ append.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function appendText(){
+    var text = document.getElementById('textField1').value;
+    document.getElementById('div1').innerHTML += text;
+}
+
